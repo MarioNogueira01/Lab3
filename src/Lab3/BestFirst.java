50cb0bf9d09500e998f66822344017fae029e99f
Mário Nogueira <71707492+MarioNogueira01@users.noreply.github.com>
2023-10-19 15:22:43 +0100

    começo de criação do algoritmo

--- src/Lab3/BestFirst.java
+++ src/Lab3/BestFirst.java
@@ -13,12 +13,16 @@ public class BestFirst {
         private State father;
         private double g;
 
+        private int prof = -1;
+
+
         /**
          * Construtor for State class
          */
         public State(Ilayout l, State n) {
             layout = l;
             father = n;
+            prof ++;
             if (father != null)
                 g = father.g + l.getG();
             else g = 0;
@@ -81,15 +85,21 @@ public class BestFirst {
      * Solves a problem using the IDA* algorithm.
      */
     final public Iterator<State> solve(Ilayout s, Ilayout goal) {
+
         objective = goal;
         abertos = new PriorityQueue<>(10,
                 (s1, s2) -> (int) Math.signum(s1.getG() - s2.getG()));
         fechados = new HashMap<>();
         abertos.add(new State(s, null));
+        int cut = 0;
         List<State> sucs;
 
         while (!abertos.isEmpty()) {
             this.actual = abertos.poll();
+            if(abertos.isEmpty()){
+
+            }
+
             if (this.actual.layout.isGoal(objective)) {// layout.equals(objective)) { //  verificar: usar isgoal()->Board
                 List<State> result = new ArrayList<>();
                 result.add(this.actual);
@@ -100,6 +110,7 @@ public class BestFirst {
                 result.remove(result.remove(0));
                 return result.listIterator();
             } else {
+                if(actual.prof <= )
                 sucs = this.sucessores(actual);
                 fechados.put(actual.layout, actual);
                 for (State item : sucs) {
