581d4e7b91701d6ac9eb2b2d957c8f549ce4c54b
Mário Nogueira <71707492+MarioNogueira01@users.noreply.github.com>
2023-10-29 20:06:45 +0000

    tentativa de mudança de heuristica
    Mudança de heuristica mas sem saber o que é o TripleA g = (TripleA) goal;

--- src/Lab3/sequence.java
+++ src/Lab3/sequence.java
@@ -41,7 +41,37 @@ public class sequence implements Ilayout {
     }
 
     private int heuristic(int i, int goalNum) {
-        return Math.abs(goalNum - i);
+        TripleA g = (TripleA) goal;
+        int num = this.num;
+        if(num == 0) return Integer.MAX_VALUE; // temp
+        int result = 0;
+
+        double exp = 0;
+        if(num < 0){
+            exp = Math.round(TripleA.log2((double)goalNum/(num-0.5)));
+        }
+        else{
+            exp = Math.round(TripleA.log2((double)goalNum/(num+0.5)));
+        }
+        if(exp <= 0){
+            double dist = goalNum - num;
+            if (num < 0){
+                result = (int) (Math.abs(dist)*2);
+            }
+            else{
+                result = (int) Math.abs(dist);
+            }
+        }
+        else{
+            double y = (goalNum/Math.pow(2,exp) - num);
+            double y_2 = (goalNum/Math.pow(2,exp-1) - num);
+
+            y = y < 0 ? Math.abs(y *2) : Math.abs(y);
+            y_2 = y_2 < 0 ? Math.abs(y_2 *2) : Math.abs(y_2);
+
+            result = (int) Math.min(exp*3 + y, (exp-1)*3 + y_2);
+        }
+        return result;
     }
     //TODO: acabar a heuristica
     @Override
